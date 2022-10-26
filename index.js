5ba0e3a9ef3ef7d75b6a98e1847fbfa7d50e0814
Nir Elbaz <nire0510@gmail.com>
2022-10-26 14:11:03 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+const users = ["Bob Smith", "Guillaume Bibeau"];
+
+export default (req, res) => {
+  const { id } = req.query;
+  res.status(200).json({ user: users[Number(id)] });
+};
