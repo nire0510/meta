ab2fa7730a524eac04a8f2e2a0c03b8ba62f3d51
Nir Elbaz <nire0510@gmail.com>
2022-10-26 14:14:31 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-const users = ["Bob Smith", "Guillaume Bibeau"];
-
-export default (req, res) => {
-  const { id } = req.query;
-  res.status(200).json({ user: users[Number(id)] });
-};
